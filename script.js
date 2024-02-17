e53a0867ee3f8f0df1e63139550691d1ec4068c3
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-02-17 22:37:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function stringChop(str, size) {
+function stringChop(str, length) {
   // your code here
+	if (str === null) {
+		return [];
+	}
+
+	const result = [];
+	for (let i = 0; i < str.length; i++) {
+		result.puch(str.substring(i, i + length));
+		i += length-1;
+	}
+	return result;
 }
 
+console.log(chunkString("Hello, world!", 5));
+console.log(chunkString("12345", 2));
+console.log(chunkString("abc", 5));
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
+// const str = prompt("Enter String.");
+// const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
